e469e38ed4074843738268cbfe6c6d318c0cca11
leo-oxu <shenxiaozhuxu@gmail.com>
2025-09-20 14:50:36 -0400

    reproduce example3

---
+++ activity1_3.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime,timezone
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.now(timezone.utc))
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name, current_time=datetime.now(timezone.utc))
